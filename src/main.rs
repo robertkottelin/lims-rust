1084fa5bcf84e04309630f190c709cd1f7a266d3
robertkottelin <robert.kottelin@gmail.com>
2023-05-15 11:12:54 +0000

    updates

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ struct Sample {
     description: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct SamplesRequest {
+    samples: Vec<SampleRequest>,
+}
+
 struct Analysis {
     id: i32,
     sample_id: i32,
@@ -101,29 +106,39 @@ struct QualityControlResponse {
     actual_result: String,
 }
 
-fn init_db() -> Result<Connection> {
-    let conn = Connection::open("lims.db")?;
+async fn add_sample_handler(
+    conn: web::Data<Connection>,
+    item: web::Json<SampleRequest>,
+) -> HttpResponse {
+    let new_sample = Sample {
+        id: 0, // Replace with proper id generation
+        name: item.name.clone(),
+        description: item.description.clone(),
+    };
+    match add_sample(&conn, &new_sample) {
+        Ok(_) => HttpResponse::Created().json(SampleResponse {
+            id: new_sample.id,
+            name: new_sample.name,
+            description: new_sample.description,
+        }),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS samples (
-            id              INTEGER PRIMARY KEY,
-            name            TEXT NOT NULL,
-            description     TEXT NOT NULL
-        )",
-        params![],
-    )?;
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS analyses (
-            id              INTEGER PRIMARY KEY,
-            sample_id       INTEGER NOT NULL,
-            result          TEXT NOT NULL,
-            FOREIGN KEY(sample_id) REFERENCES samples(id)
-        )",
-        params![],
-    )?;
-
-    Ok(conn)
+async fn get_samples_handler(conn: web::Data<Connection>) -> HttpResponse {
+    match get_samples(&conn, None) {
+        Ok(samples) => HttpResponse::Ok().json(
+            samples
+                .into_iter()
+                .map(|sample| SampleResponse {
+                    id: sample.id,
+                    name: sample.name,
+                    description: sample.description,
+                })
+                .collect::<Vec<SampleResponse>>(),
+        ),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
 }
 
 async fn add_analysis_handler(
@@ -158,6 +173,35 @@ async fn update_sample_description_handler(
 async fn delete_sample_handler(conn: web::Data<Connection>, item: web::Path<i32>) -> HttpResponse {
     match delete_sample(&conn, item.into_inner()) {
         Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+async fn add_samples_handler(
+    conn: web::Data<Connection>,
+    items: web::Json<SamplesRequest>,
+) -> HttpResponse {
+    let new_samples = items
+        .samples
+        .iter()
+        .map(|item| Sample {
+            id: 0, // Replace with proper id generation
+            name: item.name.clone(),
+            description: item.description.clone(),
+        })
+        .collect::<Vec<Sample>>();
+
+    match add_samples(&conn, &new_samples) {
+        Ok(_) => HttpResponse::Created().json(
+            new_samples
+                .into_iter()
+                .map(|sample| SampleResponse {
+                    id: sample.id,
+                    name: sample.name,
+                    description: sample.description,
+                })
+                .collect::<Vec<SampleResponse>>(),
+        ),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
@@ -310,250 +354,29 @@ async fn record_quality_control_handler(
     }
 }
 
-// Basic CRUD for samples
-fn add_sample(conn: &Connection, sample: &Sample) -> Result<()> {
+fn init_db() -> Result<Connection> {
+    let conn = Connection::open("lims.db")?;
+
     conn.execute(
-        "INSERT INTO samples (id, name, description) VALUES (?1, ?2, ?3)",
-        params![sample.id, sample.name, sample.description],
-    )?;
-    Ok(())
-}
-
-fn add_analysis(conn: &Connection, analysis: &Analysis) -> Result<()> {
-    conn.execute(
-        "INSERT INTO analyses (id, sample_id, result) VALUES (?1, ?2, ?3)",
-        params![analysis.id, analysis.sample_id, analysis.result],
-    )?;
-    Ok(())
-}
-
-fn update_sample_description(
-    conn: &Connection,
-    sample_id: i32,
-    new_description: String,
-) -> Result<usize, rusqlite::Error> {
-    conn.execute(
-        "UPDATE samples SET description = ?1 WHERE id = ?2",
-        params![new_description, sample_id],
-    )
-}
-
-fn delete_sample(conn: &Connection, sample_id: i32) -> Result<usize, rusqlite::Error> {
-    conn.execute("DELETE FROM samples WHERE id = ?1", params![sample_id])
-}
-
-// Querying and Reporting
-fn get_samples(conn: &Connection, sample_id: Option<i32>) -> Result<Vec<Sample>, rusqlite::Error> {
-    let mut stmt =
-        conn.prepare("SELECT id, name, description FROM samples WHERE (?1 IS NULL OR id = ?1)")?;
-
-    let map_row = |row: &Row| {
-        Ok(Sample {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            description: row.get(2)?,
-        })
-    };
-
-    let rows = match sample_id {
-        Some(id) => stmt.query_map(params![id], map_row)?,
-        None => stmt.query_map([], map_row)?,
-    };
-
-    let mut samples = Vec::new();
-    for row in rows {
-        samples.push(row?);
-    }
-    Ok(samples)
-}
-
-// Batch Operations
-fn add_samples(conn: &mut Connection, samples: &Vec<Sample>) -> Result<(), rusqlite::Error> {
-    let tx = conn.transaction()?;
-
-    for sample in samples {
-        tx.execute(
-            "INSERT INTO samples (id, name, description) VALUES (?1, ?2, ?3)",
-            params![sample.id, sample.name, sample.description],
-        )?;
-    }
-
-    tx.commit()?;
-    Ok(())
-}
-
-// User management
-struct User {
-    id: i32,
-    username: String,
-    password: String, // TODO: hash and .env
-    role: String,
-}
-
-fn add_user(conn: &Connection, user: &User) -> Result<(), rusqlite::Error> {
-    conn.execute(
-        "INSERT INTO users (id, username, password, role) VALUES (?1, ?2, ?3, ?4)",
-        params![user.id, user.username, user.password, user.role],
+        "CREATE TABLE IF NOT EXISTS samples (
+            id              INTEGER PRIMARY KEY,
+            name            TEXT NOT NULL,
+            description     TEXT NOT NULL
+        )",
+        params![],
     )?;
 
-    Ok(())
-}
-
-fn update_user_role(
-    conn: &Connection,
-    user_id: i32,
-    new_role: String,
-) -> Result<usize, rusqlite::Error> {
     conn.execute(
-        "UPDATE users SET role = ?1 WHERE id = ?2",
-        params![new_role, user_id],
-    )
-}
-
-fn delete_user(conn: &Connection, user_id: i32) -> Result<usize, rusqlite::Error> {
-    conn.execute("DELETE FROM users WHERE id = ?1", params![user_id])
-}
-
-// Inventory Management
-struct InventoryItem {
-    id: i32,
-    name: String,
-    quantity: i32,
-}
-
-fn add_inventory_item(conn: &Connection, item: &InventoryItem) -> Result<(), rusqlite::Error> {
-    conn.execute(
-        "INSERT INTO inventory (id, name, quantity) VALUES (?1, ?2, ?3)",
-        params![item.id, item.name, item.quantity],
+        "CREATE TABLE IF NOT EXISTS analyses (
+            id              INTEGER PRIMARY KEY,
+            sample_id       INTEGER NOT NULL,
+            result          TEXT NOT NULL,
+            FOREIGN KEY(sample_id) REFERENCES samples(id)
+        )",
+        params![],
     )?;
 
-    Ok(())
-}
-
-fn update_inventory_item_quantity(
-    conn: &Connection,
-    item_id: i32,
-    new_quantity: i32,
-) -> Result<usize, rusqlite::Error> {
-    conn.execute(
-        "UPDATE inventory SET quantity = ?1 WHERE id = ?2",
-        params![new_quantity, item_id],
-    )
-}
-
-fn delete_inventory_item(conn: &Connection, item_id: i32) -> Result<usize, rusqlite::Error> {
-    conn.execute("DELETE FROM inventory WHERE id = ?1", params![item_id])
-}
-
-// Scheduling and Workflow Management
-struct Test {
-    id: i32,
-    name: String,
-    description: String,
-}
-
-struct Schedule {
-    id: i32,
-    sample_id: i32,
-    test_id: i32,
-    scheduled_time: String, // Note: you'd likely want to use a DateTime type in a real application
-}
-
-fn add_test(conn: &Connection, test: &Test) -> Result<(), rusqlite::Error> {
-    conn.execute(
-        "INSERT INTO tests (id, name, description) VALUES (?1, ?2, ?3)",
-        params![test.id, test.name, test.description],
-    )?;
-
-    Ok(())
-}
-
-fn schedule_test(conn: &Connection, schedule: &Schedule) -> Result<(), rusqlite::Error> {
-    conn.execute(
-        "INSERT INTO schedule (id, sample_id, test_id, scheduled_time) VALUES (?1, ?2, ?3, ?4)",
-        params![
-            schedule.id,
-            schedule.sample_id,
-            schedule.test_id,
-            schedule.scheduled_time
-        ],
-    )?;
-
-    Ok(())
-}
-
-// Quality Control and Assurance
-struct QualityControl {
-    id: i32,
-    sample_id: i32,
-    test_id: i32,
-    expected_result: String,
-    actual_result: String,
-}
-
-fn record_quality_control(conn: &Connection, qc: &QualityControl) -> Result<(), rusqlite::Error> {
-    conn.execute(
-        "INSERT INTO quality_control (id, sample_id, test_id, expected_result, actual_result) VALUES (?1, ?2, ?3, ?4, ?5)",
-        params![qc.id, qc.sample_id, qc.test_id, qc.expected_result, qc.actual_result],
-    )?;
-
-    Ok(())
-}
-
-// Your existing structs and functions here...
-
-// For every struct, define a request and response version, e.g.:
-
-#[derive(Serialize, Deserialize)]
-struct SampleRequest {
-    name: String,
-    description: String,
-}
-
-#[derive(Serialize)]
-struct SampleResponse {
-    id: i32,
-    name: String,
-    description: String,
-}
-
-// And so on for Analysis, User, InventoryItem, Test, Schedule, QualityControl...
-
-// Then, for each function that you want to expose as an HTTP endpoint, define a handler function:
-
-async fn add_sample_handler(
-    conn: web::Data<Connection>,
-    item: web::Json<SampleRequest>,
-) -> HttpResponse {
-    let new_sample = Sample {
-        id: 0, // Replace with proper id generation
-        name: item.name.clone(),
-        description: item.description.clone(),
-    };
-    match add_sample(&conn, &new_sample) {
-        Ok(_) => HttpResponse::Created().json(SampleResponse {
-            id: new_sample.id,
-            name: new_sample.name,
-            description: new_sample.description,
-        }),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
-}
-
-async fn get_samples_handler(conn: web::Data<Connection>) -> HttpResponse {
-    match get_samples(&conn, None) {
-        Ok(samples) => HttpResponse::Ok().json(
-            samples
-                .into_iter()
-                .map(|sample| SampleResponse {
-                    id: sample.id,
-                    name: sample.name,
-                    description: sample.description,
-                })
-                .collect::<Vec<SampleResponse>>(),
-        ),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
+    Ok(conn)
 }
 
 // Create server
